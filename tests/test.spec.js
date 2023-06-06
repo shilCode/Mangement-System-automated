87180651f5a0d7dee1aacde05a4f586d238607d7
ShilCode <2850542@gmail.com>
2023-06-06 18:14:25 +0200

    Day 2 of changes

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,5 +1,5 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect, chromium } = require('@playwright/test');
 let page;
 
 test.beforeAll( async ({browser})=>{
@@ -39,7 +39,50 @@ test.describe('LOGIN',async ()=>{
     await expect(page.locator('[alt="profile picture"]').nth(0)).toBeInViewport()
   
   });
+
+
+  test('forget password',async ()=>{
+
+    await page.locator('[class*="orangehrm-login-forgot-header"]').click()
+    await page.locator('[name="username"]').type('abc')
+    await page.locator('[type="submit"]').click()
+    await expect(await page.locator('[class*="orangehrm-forgot-password-title"]')).toHaveText("Reset Password link sent successfully")
+  
+  })
 } )
+
+
+
+test.describe.only('Sidebar',async()=>{
+  test('Dashboard needs to be landing page after login',async()=>{
+
+    //valid loging - need to delete this while changing things to POM
+
+    await page.locator('[placeholder="Username"]').type('Admin')
+    await page.locator('[name="password"]').type('admin123')
+    await page.locator('[type="submit"]').click()
+    await expect(page.locator('[alt="profile picture"]').nth(0)).toBeInViewport()
+
+    
+    //making sure after login Dashboard is present after login
+    await expect(page.locator('[class*=oxd-text--h6]')).toContainText('Dashboard')
+
+  })
+  test('Verify that sidebar can be hidden',async()=>{
+
+    
+    await page.locator('[placeholder="Username"]').type('Admin')
+    await page.locator('[name="password"]').type('admin123')
+    await page.locator('[type="submit"]').click()
+    await expect(page.locator('[alt="profile picture"]').nth(0)).toBeInViewport()
+
+    await page.locator('[class*="oxd-main-menu-button"]').click()
+
+
+  })
+
+  
+})
 
 
 
